feda70e082275cd0d07d65819ae8642083df2658
bouzuya <m@bouzuya.net>
2016-05-15 18:46:01 +0900

    extract count component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,40 +1,11 @@
 import React from 'react';
-import { bindActionCreators, Dispatch, Action } from 'redux';
-import { connect } from 'react-redux';
-import { State } from '../types/';
-import { create as createDecrementAction } from '../actions/decrement';
-import { create as createIncrementAction } from '../actions/increment';
+import { Count } from '../components/count';
 
-type Props = DataProps & ActionProps;
-type DataProps = {
-  count: number;
-};
-type ActionProps = {
-  decrement: typeof createDecrementAction
-  increment: typeof createIncrementAction
-};
+type Props = {};
 
-const component = ({ count, decrement, increment }: Props): JSX.Element =>
-  <div className="count">
-    <p>
-      {
-        count
-      }
-    </p>
-    <button onClick={decrement}>-</button>
-    <button onClick={increment}>+</button>
+const component = (): JSX.Element =>
+  <div className="main">
+    <Count />
   </div>;
 
-const connectedComponent = connect(
-  ({ count: { count } }: State): DataProps => {
-    return { count };
-  },
-  (dispatch: Dispatch<Action>): ActionProps => {
-    return bindActionCreators({
-      decrement: createDecrementAction,
-      increment: createIncrementAction
-    }, dispatch);
-  }
-)(component);
-
-export { connectedComponent as App };
+export { component as App };
---
+++ src/components/count.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { bindActionCreators, Dispatch, Action } from 'redux';
+import { connect } from 'react-redux';
+import { State } from '../types/';
+import { create as createDecrementAction } from '../actions/decrement';
+import { create as createIncrementAction } from '../actions/increment';
+
+type Props = DataProps & ActionProps;
+type DataProps = {
+  count: number;
+};
+type ActionProps = {
+  decrement: typeof createDecrementAction
+  increment: typeof createIncrementAction
+};
+
+const component = ({ count, decrement, increment }: Props): JSX.Element =>
+  <div className="count">
+    <p>
+      {
+        count
+      }
+    </p>
+    <button onClick={decrement}>-</button>
+    <button onClick={increment}>+</button>
+  </div>;
+
+const connectedComponent = connect(
+  ({ count: { count } }: State): DataProps => {
+    return { count };
+  },
+  (dispatch: Dispatch<Action>): ActionProps => {
+    return bindActionCreators({
+      decrement: createDecrementAction,
+      increment: createIncrementAction
+    }, dispatch);
+  }
+)(component);
+
+export { connectedComponent as Count };
